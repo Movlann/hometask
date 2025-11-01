f08c812b40cbe46a10ab95c3af4d2aaab2f6b635
Movlann <movlan369@gmail.com>
2025-11-02 01:20:01 +0400

    Add files via upload

---
+++ lvl1_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int x;
+int main (){
+    cout << "Eded daxil edin: ";
+    cin >> x;
+    cout << "Kvadrati: " << x * x << endl;
+    cout << "Kubu: " << x * x * x << endl;
+}
