e24df4ecaf6c18661d3b9c658cd0a39d432ee29f
Movlann <movlan369@gmail.com>
2025-11-02 02:08:07 +0400

    Add files via upload

---
+++ lvl1_8.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int a;
+int main (){
+    cout << "Kvadratin terefini daxil edin: ";
+    cin >> a;
+    cout << "Sahe: " << a * a << endl;
+    cout << "Perimetr: " << 4 * a << endl;
+}
